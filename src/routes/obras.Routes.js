b9d1a2295a650ae3fe07a0a41c73feb0be2cd667
sr <srzzuares@gmail.com>
2023-06-27 12:13:02 -0600

    doc Api Finally

--- src/routes/obras.Routes.js
+++ src/routes/obras.Routes.js
@@ -42,24 +42,138 @@ const OCLL = require('../controllers/obrasControllers')
  *         - nombreAutor
  *         - correoAutor
  *        example:
- *         idObra:1
- *         nombreObra:La Soledad
- *         tamanoObra:Mediano 40x80
- *         tecnicaObra:Oleo con acrilico
- *         materialesObra:Pintura, Lienzo
- *         valorEconomicoObra:1000
- *         nombreAutor:Crystian
- *         telefonoAutor:7641112222
- *         correoAutor:crys@gmail.com
+ *         idObra: 1
+ *         nombreObra: La Soledad
+ *         tamanoObra: Mediano 40x80
+ *         tecnicaObra: Oleo con acrilico
+ *         materialesObra: Pintura, Lienzo
+ *         valorEconomicoObra: 1000
+ *         nombreAutor: Crystian
+ *         telefonoAutor: 7641112222
+ *         correoAutor: crys@gmail.com
  */
 
 
 
-
-
+/**
+ *  @swagger
+ *  /:
+ *  get:
+ *      summary: Retorna todas las obras creadas
+ *      tags: [User]
+ *      responses:
+ *          200:
+ *              description: Todas las obras creadas
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/User'
+ */
 rooteable.get('/', OCLL.obtenerObra)
+
+/**
+ *  @swagger
+ *  /obtAct/{idOb}:
+ *  get:
+ *      summary: Retorna una id de Obra
+ *      tags: [User]
+ *      parameters:
+ *          - in: path
+ *            name: idOb
+ *            required: true
+ *            schema:
+ *                type: string
+ *            description: Identificacion de la obra
+ *      responses:
+ *          200:
+ *              description: Todas las obras creadas
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/User'
+ *          404:
+ *              description: Identificacion no encontrada
+ */
 rooteable.get('/obtAct/:idOb', OCLL.obtenerOneObra)
+
+/**
+ *  @swagger
+ *  /crear:
+ *      post:
+ *          summary: Crear una obra de arte
+ *          tags: [User]
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/User'
+ *          responses:
+ *              200:
+ *                  description: Success created 
+ */
 rooteable.post('/crear', OCLL.guardarObras)
+
+/**
+ *  @swagger
+ *  /eliminar/{idOb}:
+ *  put:
+ *      summary: actualizar una obra
+ *      tags: [User]
+ *      parameters:
+ *          - in: path
+ *            name: idOb
+ *            required: true
+ *            schema:
+ *                type: string
+ *            description: Identificacion de la obra
+ *      requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: Obra actualizada
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/User'
+ *          404:
+ *              description: Identificacion no encontrada
+ */
 rooteable.put('/actualizar/:idOb', OCLL.actualizaObra)
+
+/**
+ *  @swagger
+ *  /eliminar/{idOb}:
+ *  delete:
+ *      summary: Elimina una obra
+ *      tags: [User]
+ *      parameters:
+ *          - in: path
+ *            name: idOb
+ *            required: true
+ *            schema:
+ *                type: string
+ *            description: Identificacion de la obra
+ *      responses:
+ *          200:
+ *              description: Obra Eliminada
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/User'
+ *          400:
+ *              description: Identificacion no encontrada
+ */
 rooteable.delete('/eliminar/:idOb', OCLL.eliminaObra)
 module.exports = rooteable;
